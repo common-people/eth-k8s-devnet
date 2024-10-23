1d14f082c503d44d335d0f27a4125f211387e2b4
Christopher <agj017@gmail.com>
2024-10-23 13:53:28 +0900

    add geth el ingress

--- genesis-generator/build-docker.sh
+++ genesis-generator/build-docker.sh
@@ -1,5 +1,7 @@
 docker login
 
-docker build --no-cache -t agj017/openssl-curl:latest .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 --push -t agj017/openssl-curl:latest .
+# docker buildx build --no-cache --platform linux/amd64,linux/arm64 -t agj017/openssl-curl:latest --push .
 
 docker push agj017/openssl-curl:latest
+
